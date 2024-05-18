41c3d7d52a390b97f1e90e6add64f32cd9ba9ab1
Manfred Fong <mafong081102@gmail.com>
2024-05-17 20:12:04 -0400

    Implemented default navBar version 1.0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Contact from './components/Contact'
+import Home from './pages/Home'
+import About from './components/About'
 
 function App() {
+  
   return (
-    <></>
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path="/about" element={<About />} />
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+      </Routes>
+    </>
   );
 }
 
