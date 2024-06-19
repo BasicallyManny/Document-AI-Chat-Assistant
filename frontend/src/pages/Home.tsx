5dcad03f386b94d24f33c531d5f71e83e446e353
Manfred Fong <mafong081102@gmail.com>
2024-06-19 14:14:51 -0400

    added background stock video to assets

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,18 @@
-export default function Home(){
-    return(
-        <>Home Page</>
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Row, Container, Col } from 'react-bootstrap';
+
+export default function Home() {
+    return (
+        <Container>
+            <section id="homeComponent">
+                <>Home Page</>
+            </section>
+            <section>
+                <>About Component</>
+            </section>
+            <section id="routeMessageBoard">
+                <>Try App</>
+            </section>
+        </Container>
     );
 }
